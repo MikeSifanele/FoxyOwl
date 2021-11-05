49ff3713827caed5a5a353bfd4d8015d2f87a3cb
mike <mike.sifanele2.0@gmail.com>
2021-11-05 17:17:57 +0200

    grouped form into regions

--- FoxyOwl/Form1.cs
+++ FoxyOwl/Form1.cs
@@ -15,6 +15,7 @@ namespace FoxyOwl
 {
     public partial class Form1 : Form
     {
+        #region private fields
         private string _symbol = "Volatility 75 Index";
         private int _lotPercent = 10;
         private int _period = 3;
@@ -24,6 +25,8 @@ namespace FoxyOwl
 
         private Timer CandleTimer = null;
         private bool _isCandleTimerSynced = false;
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -37,6 +40,169 @@ namespace FoxyOwl
             CandleTimer_Tick(null, null);
 
             CandleTimer.Tick += CandleTimer_Tick;
+        }
+
+        #region Navigation events
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        #region Click events
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            if (!MqlHelper.Instance.OpenBuyOrder(_symbol, MqlHelper.Instance.GetLotSize(_symbol, _lotPercent), "Bought manually"))
+            {
+
+            }
+
+            chartPanel.Refresh();
+        }
+
+        private void btnSell_Click(object sender, EventArgs e)
+        {
+            if (!MqlHelper.Instance.OpenSellOrder(_symbol, MqlHelper.Instance.GetLotSize(_symbol, _lotPercent), "Sold manually"))
+            {
+
+            }
+
+            chartPanel.Refresh();
+        }
+
+        private void btnPositionsCloseAll_Click(object sender, EventArgs e)
+        {
+            if (!MqlHelper.Instance.PositionCloseAll(_symbol))
+            {
+
+            }
+
+            chartPanel.Refresh();
+        }
+
+        private void cbAutoTrade_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                btnBack.Enabled = btnForward.Enabled = btnBuy.Enabled = btnSell.Enabled = btnPositionsCloseAll.Enabled = cbAutoTrade.Checked;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        #endregion
+
+        #region Custom methods
+        private int XPoint;
+        private void LoadChart(List<MacdRates> candlesticks)
+        {
+            try
+            {
+                XPoint = 0;
+
+                chartPanel.CreateGraphics().Clear(chartPanel.BackColor);
+
+                var candlesMaxHeight = candlesticks.Max(x => x.High);
+                var candlesMinHeight = candlesticks.Min(x => x.Low);
+
+                var points = MqlHelper.Instance.GetPoints(_symbol);
+
+                var maxPoints = Math.Floor((candlesMaxHeight - candlesMinHeight) * points);
+                var candlePadding = candlesticks[0].CandleGraphics.WickWidth;
+
+                PlotOpenPositions(_symbol, points, maxPoints, candlesMaxHeight);
+
+                for (int i = 0; i < candlesticks.Count; i++)
+                {
+                    XPoint += candlesticks[i].CandleGraphics.BodyWidth + candlePadding;
+
+                    var bodyHeight = ChartConvert.ToRelativeValue(candlesticks[i].CandleGraphics.BodyHeight, maxPoints, chartPanel.Height);
+                    var wickHeight = ChartConvert.ToRelativeValue(candlesticks[i].CandleGraphics.WickHeight, maxPoints, chartPanel.Height);
+
+                    //TODO: add body & wick Offset.
+                    var bodyOffset = ChartConvert.ToRelativeValue((int)Math.Floor((candlesMaxHeight - Math.Max(candlesticks[i].Open, candlesticks[i].Close)) * points), maxPoints, chartPanel.Height);
+                    var wickOffset = ChartConvert.ToRelativeValue((int)Math.Floor((candlesMaxHeight - candlesticks[i].High) * points), maxPoints, chartPanel.Height);
+
+                    candlesticks[i].SetCandleDimensions(points: points);
+
+                    var candleGraphics = chartPanel.CreateGraphics();
+
+                    candleGraphics.FillRectangle(candlesticks[i].CandleGraphics.Brush, new Rectangle(XPoint, bodyOffset, candlesticks[i].CandleGraphics.BodyWidth, bodyHeight));
+                    candleGraphics.FillRectangle(candlesticks[i].CandleGraphics.Brush, new Rectangle(XPoint + candlePadding, wickOffset, candlesticks[i].CandleGraphics.WickWidth, wickHeight));
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void PlotOpenPositions(string symbol, int points, double maxPoints, float maxHeight)
+        {
+            try
+            {
+                var positionsGraphics = chartPanel.CreateGraphics();
+
+                var positionsOpen = MqlHelper.Instance.GetTradePositions(symbol);
+
+                int yPoint = 0;
+
+                for (int i = 0; i < positionsOpen.Length; i++)
+                {
+                    yPoint = ChartConvert.ToRelativeValue((int)Math.Floor((maxHeight - positionsOpen[i].PriceOpen) * points), maxPoints, chartPanel.Height);
+                    positionsGraphics.DrawLine(new Pen(Color.Green, 1) { DashStyle = DashStyle.Dash }, new Point(0, y: yPoint), new Point(chartPanel.Width, yPoint));
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        #endregion
+
+        #region Auto events
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _macdRates = MqlHelper.Instance.GetMacdRates(_symbol, period: _period, count: _numChartCandles);
+
+                LoadChart(_macdRates);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                LoadChart(_macdRates);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void chartPanel_ClientSizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                mainPanel_Paint(null, null);
+            }
+            catch (Exception)
+            {
+
+            }
         }
 
         private void CandleTimer_Tick(object sender, EventArgs e)
@@ -82,163 +248,9 @@ namespace FoxyOwl
             }
             catch (Exception)
             {
-                
-            }
-        }
-
-        private void btnBack_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnForward_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnBuy_Click(object sender, EventArgs e)
-        {
-            if (!MqlHelper.Instance.OpenBuyOrder(_symbol, MqlHelper.Instance.GetLotSize(_symbol, _lotPercent), "Bought manually"))
-            {
-
-            }
-
-            chartPanel.Refresh();
-        }
-
-        private void btnSell_Click(object sender, EventArgs e)
-        {
-            if (!MqlHelper.Instance.OpenSellOrder(_symbol, MqlHelper.Instance.GetLotSize(_symbol, _lotPercent), "Sold manually"))
-            {
-
-            }
-
-            chartPanel.Refresh();
-        }
-
-        private void btnPositionsCloseAll_Click(object sender, EventArgs e)
-        {
-            if (!MqlHelper.Instance.PositionCloseAll(_symbol))
-            {
-
-            }
-
-            chartPanel.Refresh();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                _macdRates = MqlHelper.Instance.GetMacdRates(_symbol, period: _period, count: _numChartCandles);
-
-                LoadChart(_macdRates);
-            }
-            catch (Exception)
-            {
 
             }
         }
-
-        private void mainPanel_Paint(object sender, PaintEventArgs e)
-        {
-            try
-            {
-                LoadChart(_macdRates);
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-
-        private int XPoint;
-        private void LoadChart(List<MacdRates> candlesticks)
-        {
-            try
-            {
-                XPoint = 0;
-
-                chartPanel.CreateGraphics().Clear(chartPanel.BackColor);
-
-                var candlesMaxHeight = candlesticks.Max(x => x.High);
-                var candlesMinHeight = candlesticks.Min(x => x.Low);
-
-                var points = MqlHelper.Instance.GetPoints(_symbol);
-
-                var maxPoints = Math.Floor((candlesMaxHeight - candlesMinHeight) * points);
-                var candlePadding = candlesticks[0].CandleGraphics.WickWidth;
-
-                PlotOpenPositions(_symbol, points, maxPoints, candlesMaxHeight);
-
-                for (int i = 0; i < candlesticks.Count; i++)
-                {
-                    XPoint += candlesticks[i].CandleGraphics.BodyWidth + candlePadding;
-
-                    var bodyHeight = ChartConvert.ToRelativeValue(candlesticks[i].CandleGraphics.BodyHeight, maxPoints, chartPanel.Height);
-                    var wickHeight = ChartConvert.ToRelativeValue(candlesticks[i].CandleGraphics.WickHeight, maxPoints, chartPanel.Height);
-
-                    //TODO: add body & wick Offset.
-                    var bodyOffset = ChartConvert.ToRelativeValue((int)Math.Floor((candlesMaxHeight - Math.Max(candlesticks[i].Open, candlesticks[i].Close)) * points), maxPoints, chartPanel.Height);
-                    var wickOffset = ChartConvert.ToRelativeValue((int)Math.Floor((candlesMaxHeight - candlesticks[i].High) * points), maxPoints, chartPanel.Height);
-
-                    candlesticks[i].SetCandleDimensions(points: points);
-
-                    var candleGraphics = chartPanel.CreateGraphics();
-
-                    candleGraphics.FillRectangle(candlesticks[i].CandleGraphics.Brush, new Rectangle(XPoint, bodyOffset, candlesticks[i].CandleGraphics.BodyWidth, bodyHeight));
-                    candleGraphics.FillRectangle(candlesticks[i].CandleGraphics.Brush, new Rectangle(XPoint + candlePadding, wickOffset, candlesticks[i].CandleGraphics.WickWidth, wickHeight));
-                }
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-        private void PlotOpenPositions(string symbol, int points, double maxPoints, float maxHeight)
-        {
-            try
-            {
-                var positionsGraphics = chartPanel.CreateGraphics();
-
-                var positionsOpen = MqlHelper.Instance.GetTradePositions(symbol);
-
-                int yPoint = 0;
-
-                for (int i = 0; i < positionsOpen.Length; i++)
-                {
-                    yPoint = ChartConvert.ToRelativeValue((int)Math.Floor((maxHeight - positionsOpen[i].PriceOpen) * points), maxPoints, chartPanel.Height);
-                    positionsGraphics.DrawLine(new Pen(Color.Green, 1) { DashStyle = DashStyle.Dash }, new Point(0, y: yPoint), new Point(chartPanel.Width, yPoint));
-                }
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-
-        private void cbAutoTrade_CheckedChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                btnBack.Enabled = btnForward.Enabled = btnBuy.Enabled = btnSell.Enabled = btnPositionsCloseAll.Enabled = cbAutoTrade.Checked;
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-
-        private void chartPanel_ClientSizeChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                mainPanel_Paint(null, null);
-            }
-            catch (Exception)
-            {
-
-            }
-        }
+        #endregion
     }
 }
